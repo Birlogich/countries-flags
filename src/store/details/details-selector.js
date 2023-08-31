f06cd0163256f5b37b71037c45c6dfa4dc2da234
Beerlogich <beerlogich@gmail.com>
2023-08-31 18:12:21 -0400

    Deleted yarn.lock

--- src/store/details/details-selector.js
+++
@@ -1,3 +0,0 @@
-export const selectCurrentCountry = (state) => state.details.currentCounty;
-export const selectDetails = (state) => state.details;
-export const selectNeighbours = (state) => state.details.neighbours;
